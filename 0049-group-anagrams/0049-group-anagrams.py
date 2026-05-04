a76f16a310c6149560e3c5cb836d6cbf78224187
Sanjeeta Das <sanjeetadas758@gmail.com>
2026-05-05 00:00:15 +0530

    Time: 22 ms (54.2%), Space: 16.3 MB (44.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,18 +5,13 @@ class Solution(object):
         :rtype: List[List[str]]
         """
 
-        from collections import defaultdict
-    
-        anagram_map = defaultdict(list)
-        
-        for word in strs:
-            count = [0] * 26
-            
-            for char in word:
-                count[ord(char) - ord('a')] += 1
-            
-            key = tuple(count)  
-            anagram_map[key].append(word)
-        
-        return list(anagram_map.values())
+        groups = {}
+
+        for s in strs:
+            key = "".join(sorted(s))
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(s)
+
+        return list(groups.values())
         
